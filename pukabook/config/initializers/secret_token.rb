f9e1cda6844470a86eeb3ea5cb449b2644c3b8d4
LittleCatBear <j.huguet9@gmail.com>
2015-08-01 22:47:39 +1200

    pukabook init

---
+++ pukabook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pukabook::Application.config.secret_token = 'c75298fe6c68b94ecb8a704d6fbfdda7a001d4d80422138b4b21a830210e390df0a57932b9410d020a5c6a89c9e4cf1fae61969782e2528cfea4c6a8e855d82c'
